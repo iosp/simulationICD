7c47253638b2c4e7b5756337573e5d60e5d88530
Robil <robil@deepspace>
2018-02-18 14:22:22 +0200

    Add wrapper for INS

--- products/ins/icd/include/InsData.h
+++ products/ins/icd/include/InsData.h
@@ -57,112 +57,224 @@ public:
         return m_simTime;
     }
 
+    void SetSimTime(const boost::posix_time::time_duration& simTime) {
+        m_simTime = simTime;
+    }
+
     const boost::posix_time::time_duration& GetUtcTime() const {
         return m_utcTime;
+    }
+
+    void SetUtcTime(const boost::posix_time::time_duration& utcTime) {
+        m_utcTime = utcTime;
     }
 
     float GetAltitude() const {
         return m_altitude;
     }
 
+    void SetAltitude(float altitude) {
+        m_altitude = altitude;
+    }
+
     float GetLatitude() const {
         return m_latitude;
+    }
+
+    void SetLatitude(float latitude) {
+        m_latitude = latitude;
     }
 
     float GetLongitude() const {
         return m_longitude;
     }
 
+    void SetLongitude(float longitude) {
+        m_longitude = longitude;
+    }
+
     float GetAzimuth() const {
         return m_azimuth;
+    }
+
+    void SetAzimuth(float azimuth) {
+        m_azimuth = azimuth;
     }
 
     float GetPitch() const {
         return m_pitch;
     }
 
+    void SetPitch(float pitch) {
+        m_pitch = pitch;
+    }
+    
     float GetRoll() const {
         return m_roll;
+    }
+
+    void SetRoll(float roll) {
+        m_roll = roll;
     }
 
     float GetAzimuthRate() const {
         return m_azimuthRate;
     }
 
+    void SetAzimuthRate(float azimuthRate) {
+        m_azimuthRate = azimuthRate;
+    }
+
     float GetNorthVelocity() const {
         return m_northVelocity;
+    }
+
+    void SetNorthVelocity(float northVelocity) {
+        m_northVelocity = northVelocity;
     }
 
     float GetEastVelocity() const {
         return m_eastVelocity;
     }
 
+    void SetEastVelocity(float eastVelocity) {
+        m_eastVelocity = eastVelocity;
+    }
+
     float GetDownVelocity() const {
         return m_downVelocity;
+    }
+
+    void SetDownVelocity(float downVelocity) {
+        m_downVelocity = downVelocity;
     }
 
     float GetDistanceTraveled() const {
         return m_distanceTraveled;
     }
 
+    void SetDistanceTraveled(float distanceTraveled) {
+        m_distanceTraveled = distanceTraveled;
+    }
+
     float GetOdometerDistance() const {
         return m_odometerDistance;
+    }
+
+    void SetOdometerDistance(float odometerDistance) {
+        m_odometerDistance = odometerDistance;
     }
 
     bool IsMotionDetected() const {
         return m_motionDetected;
     }
 
-    int GetGpsFom() const {
+    void SetMotionDetected(float motionDetected) {
+        m_motionDetected = motionDetected;
+    }
+
+    short GetGpsFom() const {
         return m_gpsFom;
     }
 
-    int GetNumOfSatelites() const {
+    void SetGpsFom(short gpsFom) {
+        m_gpsFom = gpsFom;
+    }
+
+    short GetNumOfSatelites() const {
         return m_numOfSatelites;
+    }
+
+    void SetNumOfSatelites(short numOfSatelites) {
+        m_numOfSatelites = numOfSatelites;
     }
 
     float GetHorizontalError() const {
         return m_horizontalError;
     }
 
+    void SetHorizontalError(float horizontalError) {
+        m_horizontalError = horizontalError;
+    }
+
     float GetVerticalError() const {
         return m_verticalError;
+    }
+
+    void SetVerticalError(float verticalError) {
+        m_verticalError = verticalError;
     }
 
     float GetNorthingError() const{
         return m_northingError;
     }
 
+    void SetNorthingError(float northingError) {
+        m_northingError = northingError;
+    }
+
     float GetEastingError() const{
         return m_eastingError;
+    }
+
+    void SetEastingError(float eastingError) {
+        m_eastingError = eastingError;
     }
 
     float GetAltitudeError() const{
         return m_altitudeError;
     }
 
+    void SetAltitudeError(float altitudeError) {
+        m_altitudeError = altitudeError;
+    }
+
     float GetNorthVelocityError() const {
         return m_northVelocityError;
+    }
+
+    void SetNorthVelocityError(float northVelocityError) {
+        m_northVelocityError = northVelocityError;
     }
 
     float GetEastVelocityError() const {
         return m_eastVelocityError;
     }
 
+    void SetEastVelocityError(float eastVelocityError) {
+        m_eastVelocityError = eastVelocityError;
+    }
+
     float GetDownVelocityError() const {
         return m_downVelocityError;
+    }
+
+    void SetDownVelocityError(float downVelocityError) {
+        m_downVelocityError = downVelocityError;
     }
 
     float GetAzimuthErrorEstimation() const {
         return m_azimuthErrorEstimation;
     }
 
+    void SetAzimuthErrorEstimation(float azimuthErrorEstimation) {
+        m_azimuthErrorEstimation = azimuthErrorEstimation;
+    }
+
     float GetPitchErrorEstimation() const {
         return m_pitchErrorEstimation;
     }
 
+    void SetPitchErrorEstimation(float pitchErrorEstimation) {
+        m_pitchErrorEstimation = pitchErrorEstimation;
+    }
+
     float GetRollErrorEstimation() const {
         return m_rollErrorEstimation;
+    }
+
+    void SetRollErrorEstimation(float rollErrorEstimation) {
+        m_rollErrorEstimation = rollErrorEstimation;
     }
 
     std::string toString() const;
---
+++ products/ins/wrapper/src/InsWrapper.cpp
@@ -0,0 +1,95 @@
+/*
+* InsWrapper.cpp
+* Wrapper for INS
+* Author: Binyamin Appelbaum
+* Date: 18.02.18
+*/
+
+#include "InsWrapper.h"
+#include "InsControl.h"
+
+
+InsWrapper::InsWrapper(const std::string& confFilePath) {
+    m_icd = new InsControl(confFilePath);
+}
+
+InsWrapper::~InsWrapper(){
+    delete m_icd;
+}
+
+void InsWrapper::Run() {
+    m_icd->Run();
+}
+
+void InsWrapper::SetData() {
+    m_icd->SetData(m_data);
+    // ClearCurrentData();
+}
+
+void InsWrapper::ClearCurrentData() {
+    m_data = InsData();
+}
+
+ void InsWrapper::SetTimeStamps(int simTime, int utcTime) {
+      m_data.SetSimTime(boost::posix_time::microseconds(simTime));
+      m_data.SetUtcTime(boost::posix_time::microseconds(utcTime));
+ }
+
+// INS navigation data message
+void InsWrapper::SetPose(float altitude, float longitude, float latitude) {
+    m_data.SetAltitude(altitude);
+    m_data.SetLongitude(longitude);
+    m_data.SetLatitude(latitude);
+}
+
+void InsWrapper::SetOrientation(float azimuth, float pitch, float roll) {
+    m_data.SetAzimuth(azimuth);
+    m_data.SetPitch(pitch);
+    m_data.SetRoll(roll);
+}
+
+void InsWrapper::SetAzimuthRate(float azimuthRate) {
+    m_data.SetAzimuthRate(azimuthRate);
+}
+
+void InsWrapper::SetVelocity(float northVelocity, float eastVelocity, float downVelocity) {
+    m_data.SetNorthVelocity(northVelocity);
+    m_data.SetEastVelocity(eastVelocity);
+    m_data.SetDownVelocity(downVelocity);
+}
+
+void InsWrapper::SetDistances(float distanceTraveled, float odometerDistance) {
+    m_data.SetDistanceTraveled(distanceTraveled);
+    m_data.SetOdometerDistance(odometerDistance);
+}
+
+void InsWrapper::SetMotionDetected(bool motionDetected) {
+    m_data.SetMotionDetected(motionDetected);
+}
+
+// INS internal GPS
+void InsWrapper::SetInternalGpsFields(short gpsFom, short numOfSatelites) {
+    m_data.SetGpsFom(gpsFom);
+    m_data.SetNumOfSatelites(numOfSatelites);
+}
+
+// INS Errors estimation message
+void InsWrapper::SetDirectionErrors(float horizontalError, float verticalError, float northingError, float eastingError, float altitudeError) {
+    m_data.SetHorizontalError(horizontalError);
+    m_data.SetVerticalError(verticalError);
+    m_data.SetNorthingError(northingError);
+    m_data.SetEastingError(eastingError);
+    m_data.SetAltitudeError(altitudeError);
+}
+
+void InsWrapper::SetVelocityErrors(float northVelocityError, float eastVelocityError, float downVelocityError) {
+    m_data.SetNorthVelocityError(northVelocityError);
+    m_data.SetEastVelocityError(eastVelocityError);
+    m_data.SetDownVelocityError(downVelocityError);
+}
+
+void InsWrapper::SetOrientationErrors(float azimuthErrorEstimation, float pitchErrorEstimation, float rollErrorEstimation) {
+    m_data.SetAzimuthErrorEstimation(azimuthErrorEstimation);
+    m_data.SetPitchErrorEstimation(pitchErrorEstimation);
+    m_data.SetRollErrorEstimation(rollErrorEstimation);
+}
